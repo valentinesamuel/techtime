0a28a49f6c7a102cb0e97f2b728f812d3053f7bb
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-23 09:52:01 +0000

    FEAT: Added docker files and fonts

---
+++ README.md
@@ -0,0 +1,14 @@
+# to build docker
+```
+docker build -t valentinesamuel/techtime_vetting_task-docker:1.0.0 .
+```
+-------
+# to run docker
+```
+docker run -d --rm -p 5173:5173 valentinesamuel/techtime_vetting_task-docker:1.0.0
+```
+-------
+# to pull docker image
+```
+docker pull valentinesamuel/react-test-docker:1.0.0-dev
+```
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+/**
+ * <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap" rel="stylesheet">
+ * 
+ * font-family: 'Satoshi', sans-serif;
+ * 
+ * -----------____
+ * <link href="https://api.fontshare.com/v2/css?f[]=clash-display@700,600&display=swap" rel="stylesheet">
+ * 
+ * font-family: 'Clash Display', sans-serif;
+ *  */
